23d49afd8e82d34386b1bdc93bd2d5f3f6e0c096
Samuel Lucena de Abreu <samuellucenadeabreu@hotmail.com>
2021-12-21 11:23:14 -0300

    Basic Bean Validation

--- backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
@@ -1,8 +1,13 @@
 package com.devsuperior.dscatalog.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class UserInsertDTO extends UserDTO{
 	private static final long serialVersionUID = 1L;
 
+	@Size(min = 5, max = 255, message = "Deve ter entre 5 e 255 caracteres")
+	@NotBlank(message = "Campo requerido")
 	private String password;
 
 	public UserInsertDTO() {
