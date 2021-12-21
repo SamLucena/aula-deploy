3387811184eccd0aca34422e1c0abbb62c52077d
Samuel Lucena de Abreu <samuellucenadeabreu@hotmail.com>
2021-12-21 12:37:06 -0300

    Custom ConstraintValidator

--- backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
@@ -1,12 +1,13 @@
 package com.devsuperior.dscatalog.dto;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 
+import com.devsuperior.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO{
 	private static final long serialVersionUID = 1L;
 
-	@Size(min = 5, max = 255, message = "Deve ter entre 5 e 255 caracteres")
 	@NotBlank(message = "Campo requerido")
 	private String password;
 
